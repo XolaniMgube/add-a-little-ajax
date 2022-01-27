3e842ca72ee598adbebb2bc5dfaa771532f66292
XolaniMgube <xnmgube@gmail.com>
2022-01-27 19:42:19 +0200

    reverted to express forms version

--- src/app.js
+++ src/app.js
@@ -1,74 +1,54 @@
 // requiring frameworks to use
-let express = require('express');
-let bodyParser = require('body-parser');
-
+const express = require('express');
+const bodyParser = require('body-parser');
 const {Client} = require('pg')
+
+require('dotenv').config({path: '../.env'})
+
+// postgres class that will start the connection
 const client = new Client({
-  user: "user",
-  password: "pass",
-  host: "localhost",
-  port: "5432",
-  database: "db"
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE
 })
 
-let app = express();//starting the program
+// importing database queries
+const Visitors = require('./queries')
 
-let urlencodedParser = bodyParser.urlencoded({extended: false})//for viewing express on a pug template
+let app = express(); // starting the program
+let urlencodedParser = bodyParser.urlencoded({extended: false}) // for viewing express on a pug template
 
-app.set('view engine', 'pug');
-app.use('/css', express.static('css'))
+app.set('view engine', 'pug'); // setting pug as view engine
+app.use('/css', express.static('css')) // linking css to the html
 
-app.get('/single-page-app', function(req, res) {
+// home page
+app.get('/new_visit', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-
-// app.get('/view-table', urlencodedParser, (req, res) => {
-//   res.render('successful', {data: req.body})
-// })
-
-
 // posting to new visitor path to render the html form data
-app.post('/new_visitor', urlencodedParser, function(req, res) { 
+app.post('/success', urlencodedParser, async (req, res) => { 
+  let visitorsTable = new Visitors() // creating an instance of the visitor table 
+  let newVisitorsTable = await visitorsTable.createTable() // this will create the visitor's table if it does not already exist
   
-    res.render('successful', {data: req.body});
-
-    // add new visitor function
-    async function addVisitor(name, age, date, time, assistedBy, comments) {
-      try{
-        await client.connect()
-        await client.query("BEGIN")
-        await client.query("insert into visitors (name, age, date, time, assistedby, comments) values ($1, $2, $3, $4, $5, $6) returning *", [name, age, date, time, assistedBy, comments])
-        console.log("Inserted a new row")
-        await client.query("COMMIT")
-        await client.end()
-      }
-      catch(ex){
-        console.log("Failed to add visitor " + ex)
-      }
-      finally{
-        console.log("script closed")
-        return res.rows
-      }
-    }
-
     // calling the addNewVisitor function
-    addVisitor(
+    let addNew = await visitorsTable.addVisitor(
       req.body.name,
       req.body.age,
       req.body.date,
       req.body.time,
       req.body.assistedby,
       req.body.comments
-      )
+      );
+      
+      res.render('success.pug', 
+      {
+        data: addNew[0],
+      });  
 });
 
-app.listen(5000, function() {
-  console.log('Listening on port 5000...')
+app.listen(3000, () => {
+  console.log('Listening on port 3000...')
 });
-
-
-
-
-
-
