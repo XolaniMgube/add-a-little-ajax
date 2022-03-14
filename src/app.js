f49084532f2d48f1197afa8cb9ecb0b08e56ac62
XolaniMgube <xnmgube@gmail.com>
2022-03-14 15:08:44 +0200

    added external js and css

--- src/app.js
+++ src/app.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const port = 3000
-const {client} = require("./client-connection")
-const Visitors = require('./queries')
+const port = 3000;
+const {client} = require("./client-connection");
+const Visitors = require('./queries');
+const path = require('path');
 
 const app = express();
-const urlencodedParser = bodyParser.urlencoded({extended: false})
+const urlencodedParser = bodyParser.urlencoded({extended: false});
 
-app.use('/css', express.static('css'))
+// app.use(express.json());
+// app.use(express.urlencoded());
 
-app.get('/new_visit', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static('src/public'))
 
 app.get('/single-page-app', function(req, res) {
-  res.sendFile(__dirname + '/single-page-app.html');
+  res.sendFile(path.join(__dirname, '/public/single-page-app.html'))
 });
+
+// app.get('/javascript', function (req, res) {
+//   res.sendFile(path.join(__dirname, './public/ajax.js'))
+// })
+
+// app.get('/css', function(req, res) {
+//   res.sendFile(path.join(__dirname, './public/css/style.css'))
+// })
 
 app.post('/success', urlencodedParser, async (req, res) => { 
   const visitorsTable = new Visitors() 
